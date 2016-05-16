6aad52d0212d564afa2ff00d77a8f1a6749977ef
sahloumi <sahloumi.najima@gmail.com>
2016-05-16 15:30:51 +0200

    form

--- index.php
+++ index.php
@@ -2,9 +2,40 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>TUTO PHP</title>
 </head>
 <body>
-	
+	<div align="center">
+		<h2>Inscription</h2>
+		<br /><br /><br />	
+		<form method="POST" action="">
+			<table align="right">
+				<tr>
+					<td>
+						<label for="pseudo">Pseudo:</label>
+					</td>
+					<td>
+						<input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label for="mail">Mail:</label>
+					</td>
+					<td>
+						<input type="email" placeholder="Votre mail" id="mail" name="mail">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label for="mail2">Confirmation du mail:</label>
+					</td>
+					<td>
+						<input type="email" placeholder="Confirmez votre mail" id="mail" name="mail2">
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
 </body>
 </html>
